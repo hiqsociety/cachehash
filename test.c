f868a64ed0988ec300d9392913ca6467f005e363
hiqsociety <51492452+hiqsociety@users.noreply.github.com>
2021-09-07 16:46:45 +0800

    Update test.c

--- test.c
+++ test.c
@@ -13,6 +13,24 @@
 
 cachehash *ch = NULL;
 
+void *randstring(int length) {
+    char *string = "abcdefghijklmnopqrstuvwxyz";
+    size_t stringLen = 36;
+    char *randomString;
+
+    randomString = malloc(sizeof(char) * (length +1));
+    if (!randomString) {
+        return (char*)0;
+    }
+    unsigned int key = 0;
+    for (int n = 0;n < length;n++) {
+        key = rand() % stringLen;
+        randomString[n] = string[key];
+    }
+    randomString[length] = '\0';
+    return randomString;
+}
+
 
 void testCreation(void)
 {
@@ -67,6 +85,48 @@ void testEvict(void)
 
 int main(void)
 {
+
+	char *keya;
+	char *valuea;
+	
+        clock_t start_time = clock();
+        
+        int i;
+        char *data;
+
+        ch = cachehash_init(5, NULL);
+        
+
+        for(i=0; i<TAG_LEN; i++){
+                keya = randstring(1);
+                //void *keya = randstring(2);
+                valuea = randstring(1);
+		
+		cachehash_replace(ch, keya, strlen(keya), valuea);
+//              putK(ch, keya, valuea);
+/*
+                data = cachehash_get(ch, keya, sizeof(keya));
+                cachehash_debug_dump(ch);
+
+
+                printf("value of has is %s = %s\n", cachehash_has(ch, keya, sizeof(keya)), data);
+                assert(cachehash_has(ch, keya, sizeof(keya)) == valuea);
+
+
+                keya = gen_rdm_bytestream(2);
+                cachehash_del(ch, keya, sizeof(keya));
+
+                cachehash_debug_dump(ch);
+		*/
+                free(keya);
+                free(valuea);
+
+	}
+	
+	double elapsed_time = (double)(clock() - start_time) / CLOCKS_PER_SEC;
+        printf("Done in %f seconds\n", elapsed_time);
+
+	
 	testCreation();
 	printf("--\n");
 	testPut();
